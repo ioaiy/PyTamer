7de6c58a66157423dfe8432054c89d55c05771c2
Sergey <ioaiy@users.noreply.github.com>
2020-10-06 14:13:10 +0500

    Log file

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import sys
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
